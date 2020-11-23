24fff33f7a87d01b7908143ababc370b4b75efa3
Huytue <bachvonam1996@gmail.com>
2020-11-23 21:34:25 +0700

    update

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
-# module-nodejs-package-
+# module-nodejs-package
 
 Demo Nodejs Module Package
 
-#Using npm:
+## Using npm:
+
 - npm i module-nodejs-package
 - npm i -g module-nodejs-package
 
 In index.js:
-//Load the full build
+
+## Load the full build
+
 const modules = require('module-nodejs-package')
 
-//Call functions app()
+## Call functions app()
+
 modules.app();
 
-//Call functions mongodb() create database
+## Call functions mongodb() create database
+
 modules.mongodb("ManageStudent");
 
-//Run app in command line
+## Run app in command line
+
 node index.js
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const routeStudent = require('./routes/student');
+const routeClasses = require('./routes/classes');
+const routeAdmin = require('./routes/admin');
+const routeHome = require('./routes/home');
+
 module.exports.app = () => {
-  const express = require("express"); //ket noi express
-  const app = express();
-  const bodyParser = require("body-parser");
-
-  const routeStudent = require('./routes/student');
-  const routeClasses = require('./routes/classes');
-  const routeAdmin = require('./routes/admin');
-  const routeHome = require('./routes/home');
-
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.set("view engine", "ejs");
   app.use(express.static("node_modules/module-nodejs-package/public"));
   app.set("views", "node_modules/module-nodejs-package/views");
-
   app.use("/student", routeStudent);
   app.use("/classes", routeClasses);
   app.use("/admin", routeAdmin);
@@ -22,13 +21,23 @@ module.exports.app = () => {
 }
 
 //ket noi mongodb
-module.exports.mongodb = (nameDB) => {
+module.exports.mongoLocal = (nameDB) => {
   const mongoose = require("mongoose");
   mongoose.connect("mongodb://localhost/" + nameDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => {
-    console.log("mongo connection successfully");
+    console.log("MongoDB connection successfully");
+  });
+}
+
+module.exports.mongoOnline = (urlDB) => {
+  const mongoose = require("mongoose");
+  mongoose.connect(urlDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("MongoDB connection successfully");
   });
 }
 
